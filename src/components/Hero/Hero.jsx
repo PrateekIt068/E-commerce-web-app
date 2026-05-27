bc66a5a53e6fc301db95772fcc77cd991240bbc9
Prateek Srivastava <prateekxia7028@gmail.com>
2026-05-27 15:01:10 +0530

    created navbar

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Grocery from '../../assets/grocery.png'
+
+const Hero = () => {
+  return (
+    <section>
+        <div className='max-w-[1400px] mx-auto px-10'>
+            <div>
+
+            </div>
+
+            <div>
+                <img src={Grocery} alt="Hero Image" />
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
