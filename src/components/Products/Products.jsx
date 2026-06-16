1b04f4628f00042b4af6155ebaae0706876e446c
Prateek Srivastava <prateekxia7028@gmail.com>
2026-06-16 20:56:25 +0530

    Our Process creation

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -14,7 +14,7 @@ const Products = () => {
 
     const renderCards = filteredItems.slice(0, 8).map(product=> {
         return(
-            <Cards image={product.image} name={product.name} price={product.price}/>
+            <Cards  image={product.image} name={product.name} price={product.price}/>
         )
     })
 
